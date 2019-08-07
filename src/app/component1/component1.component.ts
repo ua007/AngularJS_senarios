9f5844fd2305714edd6fe573f98274b6467a557b
utkarsh <anandutkarsh39@gmail.com>
2019-08-07 17:44:55 +0530

    updated senario 1

---
+++ src/app/component1/component1.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component implements OnInit {
+
+  public value="Utkarsh"
+  printToConsole()
+  {
+    console.log(this.value);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
